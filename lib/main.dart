1048349547bb35dcf1c783e341d9643ec2c2fcf3
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-04-30 13:28:00 +0100

    add my roots file

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:project/core/Utils/binding.dart';
-import 'package:project/core/localization/translations.dart';
-import 'package:project/core/theme/thems.dart';
-import 'package:project/features/controller/settings%20controllers/language_controller.dart';
-import 'package:project/features/controller/settings%20controllers/theme_controller.dart';
-import 'package:project/features/view/splash%20page/splash_view.dart';
-import 'package:project/myrouts.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'firebase_options.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/services.dart';
+import 'package:tradelaw/core/Utils/binding.dart';
+import 'package:tradelaw/core/theme/thems.dart';
 
 // messaging background handler
 
 void main() {
+  // Force portrait
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(TradeLaw());
 }
 
---
+++ lib/myrouts.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class Myrouts {
+  static List<GetPage<dynamic>> get getpages {
+    return [
+      // GetPage(name: "/login", page: () => LoginPage()),
+      // GetPage(name: "/signin", page: () => SigninPage()),
+    ];
+  }
+}
