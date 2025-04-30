3f3a3dab01f61372766120842be63dd5a4e7da5f
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-04-30 13:46:49 +0100

    add login and signin controller and auth view

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,42 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tradelaw/core/Utils/binding.dart';
 import 'package:tradelaw/core/localization/translations.dart';
 import 'package:tradelaw/core/theme/thems.dart';
+import 'package:tradelaw/features/view%20model/settings%20controllers/language_controller.dart';
 import 'package:tradelaw/features/view%20model/settings%20controllers/theme_controller.dart';
 import 'package:tradelaw/myrouts.dart';
 
 // messaging background handler
 
-void main() {
+void main() async {
+  final prefs = await SharedPreferences.getInstance();
+
   // Force portrait
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(TradeLaw());
+  runApp(TradeLaw(prefs: prefs));
 }
 
 class TradeLaw extends StatelessWidget {
-  const TradeLaw({super.key});
+  const TradeLaw({super.key, required this.prefs});
+  final SharedPreferences prefs;
 
   @override
   Widget build(BuildContext context) {
+    final themeController = Get.put<ThemeController>(
+      ThemeController(),
+      permanent: true,
+    );
+    final languageController = Get.put<LanguageController>(
+      LanguageController(prefs),
+      permanent: true,
+    );
+
     return GetMaterialApp(
       title: 'QuranLife',
 
