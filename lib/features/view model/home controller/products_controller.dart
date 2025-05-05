02afac467ce268bd146fea5d099134e6d1850a91
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-05-05 12:08:45 +0100

    feat(products): add ProductsController class
    Introduce the ProductsController class to manage product-related logic in the application. This class extends GetxController for state management and will be used to handle product data and interactions.

---
+++ lib/features/view model/home controller/products_controller.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tradelaw/features/view%20model/settings%20controllers/theme_controller.dart';
+
+class ProductsController extends GetxController {
+  
+}
